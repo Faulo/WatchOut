ffd37bc31c1bf8ff18d519eb0528ca0347c8a839
caphhe <caphhe1994@gmail.com>
2020-02-01 23:03:15 +0100

    missing file

--- Assets/Scripts/SimpleGear.cs
+++ Assets/Scripts/SimpleGear.cs
@@ -7,53 +7,59 @@ public class SimpleGear : MonoBehaviour
 	[SerializeField] private List<SimpleGear> meshedGears = new List<SimpleGear>();
 	[SerializeField] private List<SimpleGear> stackedGears = new List<SimpleGear>();
 	[SerializeField] private float turningSpeed = 1;
-	//[SerializeField] private float magicNumber = 6;
+	[SerializeField] private float magicNumber = 6;
 	[SerializeField] private int toothCount = 20;
 	[SerializeField] private bool powered;
+	[SerializeField] private float offsetForAngle = 0;
+	[SerializeField] private bool adjustmentMode;
+	private Vector3 rotationAxis = new Vector3(0, 1, 0);
 
 
-	public bool activatedLastFrame = true;
+	private bool activatedLastFrame = true;
 
 
 	// Start is called before the first frame update
 	void Start()
-    {
-        
-    }
+	{
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (powered)
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		if (powered)
 		{
 			TurnGear(turningSpeed, toothCount, false, transform.rotation.eulerAngles.y, transform.position);
-		} else
+		}
+		if (adjustmentMode)
 		{
-			//activatedLastFrame = true;
+			activatedLastFrame = true;
 		}
 
 
-    }
+	}
 
 	public void TurnGear(float parentSpeed, int parentToothCount, bool stacked, float syncRot, Vector3 parentPos)
 	{
 		if (gameObject.activeSelf)
 		{
-			if (activatedLastFrame)
+			if (activatedLastFrame && !stacked)
 			{
 				//transform.LookAt(parentPos);
 				activatedLastFrame = false;
 				float parentToothWidth = 360 / parentToothCount;
 				float toothWidth = 360 / toothCount;
 
-				float newAngle = Mathf.Abs(((syncRot) % parentToothWidth / parentToothWidth)) * - toothWidth + toothWidth/2;
+				float newAngle = Mathf.Abs(((syncRot) % parentToothWidth / parentToothWidth)) * -toothWidth + toothWidth / 2;
 
 				float angleBetweenGears = Vector3.Angle(Vector3.forward, transform.position - parentPos);
-				//float offsetFromGearAngle = angleBetweenGears / magicNumber * toothWidth;
-				Debug.Log(angleBetweenGears + " | " + transform.eulerAngles.y);
+				float offsetFromGearAngle = angleBetweenGears / magicNumber * toothWidth;
+				//Debug.Log(angleBetweenGears + " | " + transform.eulerAngles.y);
 
-				Debug.Log("ParentOffset: " + ((syncRot) % parentToothWidth / parentToothWidth) + " | Ratio: " + ((float)toothCount / (float)parentToothCount) + " | SyncRot: " + syncRot);
-				gameObject.transform.eulerAngles = new Vector3(0, newAngle, 0);
+				//Debug.Log("ParentOffset: " + ((syncRot) % parentToothWidth / parentToothWidth) + " | Ratio: " + ((float)toothCount / (float)parentToothCount) + " | SyncRot: " + syncRot);
+				Debug.Log(newAngle);
+				
+				gameObject.transform.localRotation = Quaternion.AngleAxis(newAngle + offsetForAngle, rotationAxis);
 			}
 			if (stacked)
 			{
@@ -62,7 +68,7 @@ public class SimpleGear : MonoBehaviour
 			else
 			{
 				turningSpeed = parentSpeed * ((float)parentToothCount / (float)toothCount);
-				
+
 			}
 			foreach (var meshedGear in meshedGears)
 			{
@@ -72,14 +78,15 @@ public class SimpleGear : MonoBehaviour
 			{
 				stackedGear.TurnGear(turningSpeed, toothCount, true, transform.rotation.eulerAngles.y, transform.position);
 			}
-			gameObject.transform.eulerAngles = new Vector3(0, gameObject.transform.rotation.eulerAngles.y + turningSpeed, 0);
 
-			if (gameObject.transform.eulerAngles.y % (360 / toothCount) < 1)
-			{
-				
-			}
+			float currentRotation = Vector3.Scale(gameObject.transform.localRotation.eulerAngles, rotationAxis).magnitude;
+			//Debug.Log(currentRotation);
+			gameObject.transform.localRotation = Quaternion.AngleAxis(currentRotation + turningSpeed, rotationAxis);
+
 		}
 	}
 
 
 }
+
+
