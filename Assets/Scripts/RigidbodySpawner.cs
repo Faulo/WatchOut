6b776d3cae1a19662d7106ac2b744e47c38ffaa8
Daniel Schulz <faulolio@gmail.com>
2020-02-01 21:05:38 +0100

    Add some awesome models

--- Assets/Scripts/RigidbodySpawner.cs
+++ Assets/Scripts/RigidbodySpawner.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+﻿using Slothsoft.UnityExtensions;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RigidbodySpawner : MonoBehaviour {
     [SerializeField]
-    Rigidbody prefab = null;
+    Rigidbody[] prefabs = null;
 
     [SerializeField, Range(0, 1000)]
     int initialSpawnCount = 0;
@@ -17,16 +18,17 @@ public class RigidbodySpawner : MonoBehaviour {
 
     IEnumerator Start() {
         for (int i = 0; i < initialSpawnCount; i++) {
-            Spawn(i);
+            Spawn(Mathf.Sqrt(i));
             yield return null;
         }
-        while (prefab) {
+        while (true) {
             Spawn();
             yield return new WaitForSeconds(interval);
         }
     }
 
-    private void Spawn(int offset = 0) {
+    private void Spawn(float offset = 0) {
+        var prefab = prefabs.RandomElement();
         var direction = Random.insideUnitCircle;
         var body = Instantiate(prefab, transform.position + offset * Random.insideUnitSphere, prefab.rotation);
         body.AddForce(force * new Vector3(direction.x, 1, direction.y), ForceMode.Acceleration);
