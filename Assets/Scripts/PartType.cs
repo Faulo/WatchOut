4fcfbd893f18d5a2aee941cb4904ec61afc95732
Daniel Schulz <faulolio@gmail.com>
2020-02-02 16:40:12 +0100

    Add PartType enum

---
+++ Assets/Scripts/PartType.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PartType {
+    GEAR01,
+    GEAR02,
+    GEAR03,
+    GEAR04,
+    GEAR05,
+    GEAR06,
+    GEAR07,
+    GEAR08,
+    GEAR09,
+    GEAR10,
+    GEAR11,
+    GEAR12,
+    GEAR13,
+    GEAR14,
+    GEAR15,
+    BALANCE_WHEEL,
+    COIL,
+    JEWEL,
+    PALETTE,
+    OTHER
+};
