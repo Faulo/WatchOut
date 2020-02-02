d883d46f076db2074f14612d246af6f85dba66a0
Daniel Schulz <faulolio@gmail.com>
2020-02-02 11:16:41 +0100

    Make parts grabbable, add special effects

---
+++ Assets/Scripts/ImpactListener.cs
@@ -0,0 +1,24 @@
+﻿using Slothsoft.UnityExtensions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ImpactListener : MonoBehaviour {
+    [SerializeField, Range(0, 10)]
+    float impactThreshold = 0;
+
+    [SerializeField]
+    ScriptableAction[] impactActions;
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.relativeVelocity.magnitude > impactThreshold) {
+            var data = new ScriptableActionData() {
+                target = gameObject,
+                collision = collision
+            };
+            impactActions.ForAll(action => action.Act(data));
+        }
+    }
+}
